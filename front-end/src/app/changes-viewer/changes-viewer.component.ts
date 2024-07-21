b4169fdb1fdb807d36f8cf2759a7d6df085e39ad
Niels <niels.breuker@outlook.com>
2024-07-21 16:41:45 +0200

    Show difference between old and new values

--- front-end/src/app/changes-viewer/changes-viewer.component.ts
+++ front-end/src/app/changes-viewer/changes-viewer.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { ChangesManagerService } from '../changes-manager.service';
 import { Option } from '../models/available-settings';
-
+import {diffWords} from "diff"
 @Component({
   selector: 'app-changes-viewer',
   templateUrl: './changes-viewer.component.html',
   styleUrl: './changes-viewer.component.scss'
 })
 export class ChangesViewerComponent {
+  public diff = diffWords;
   public isExpanded: boolean = false
   constructor(public changeManager: ChangesManagerService)
   {}
