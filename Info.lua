1ab2133cf639afd20c5bdf828d1a21262aab7195
Niels <niels.breuker@outlook.com>
2024-08-03 22:42:30 +0200

    Expanded Info.lua and added a README

--- Info.lua
+++ Info.lua
@@ -6,6 +6,21 @@ g_PluginInfo =
 {
 	Name = "ServerSettings",
 	Version = 1,
+	DisplayVersion = "0.1",
+	SourceLocation = "https://github.com/NiLSPACE/ServerSettings",
+	DownloadLocation = "https://github.com/NiLSPACE/ServerSettings/releases",
+	Description = [[Adds webadmin tabs to more easily change the server settings. This includes:
+{%list}
+{%li}Settings in the settings.ini{%/li}
+{%li}Settings in every world's world.ini{%/li}
+{%li}Uploading new plugins through the webadmin{%/li}
+{%/list}
+
+
+If settings are not relevent they are not shown. For example you won't see settings for villages if the world does not have villages enabled.
+
+A list of all the changes can be reviewed before applying them.
+	]]
 }
 
 
---
+++ README.md
@@ -0,0 +1,10 @@
+Adds webadmin tabs to more easily change the server settings. This includes: 
+ 
+ - Settings in the settings.ini 
+ - Settings in every world's world.ini 
+ - Uploading new plugins through the webadmin 
+
+
+If settings are not relevent they are not shown. For example you won't see settings for villages if the world does not have villages enabled.
+
+A list of all the changes can be reviewed before applying them.
